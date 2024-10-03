10419b09bf605302e03e306d94d8802f487624fd
Your Name <you@example.com>
2024-10-03 16:23:40 +0530

    create index page to rout.

--- js/homepage.js
+++ js/homepage.js
@@ -10,7 +10,7 @@ const beverages = [
     manufacturedDate: "2024.10.01",
     expiredDate: "2024.10.10",
     qty: 1,
-    img: "assets/11.jpg",
+    img: "../assets/11.jpg",
   },
   {
     code: "B1012",
@@ -21,7 +21,7 @@ const beverages = [
     manufacturedDate: "2024.10.05",
     expiredDate: "2024.10.06",
     qty: 1,
-    img: "assets/12.jpg",
+    img: "../assets/12.jpg",
   },
   {
     code: "B1013",
@@ -32,18 +32,18 @@ const beverages = [
     manufacturedDate: "2024.10.05",
     expiredDate: "2024.10.06",
     qty: 1,
-    img: "assets/13.jpg",
+    img: "../assets/13.jpg",
   },
   {
     code: "B1014",
     name: "Lemon Tea",
     price: 100,
     discount: 0,
-    stock: 4,
+    stock: 0,
     manufacturedDate: "2024.10.05",
     expiredDate: "2024.10.06",
     qty: 1,
-    img: "assets/14.jpg",
+    img: "../assets/14.jpg",
   },
   {
     code: "B1015",
@@ -54,7 +54,7 @@ const beverages = [
     manufacturedDate: "2024.10.05",
     expiredDate: "2025.20.06",
     qty: 1,
-    img: "assets/15.jpg",
+    img: "../assets/15.jpg",
   },
 ];
 
@@ -64,22 +64,22 @@ const burgers = [
     name: "Classic Burger (Large)",
     price: 750,
     discount: 0,
-    stock: 0,
+    stock: 2,
     manufacturedDate: "2024.09.10",
     expiredDate: "2024.10.10",
     qty: 1,
-    img: "assets/1.jpg",
+    img: "../assets/1.jpg",
   },
   {
     code: "B1002",
     name: "Classic Burger (Regular)",
     price: 1500,
     discount: 15,
-    stock: 8,
+    stock: 0,
     manufacturedDate: "2024.09.15",
     expiredDate: "2024.10.12",
     qty: 1,
-    img: "assets/2.jpg",
+    img: "../assets/2.jpg",
   },
   {
     code: "B1003",
@@ -90,7 +90,7 @@ const burgers = [
     manufacturedDate: "2024.08.20",
     expiredDate: "2024.09.30",
     qty: 1,
-    img: "assets/3.jpg",
+    img: "../assets/3.jpg",
   },
   {
     code: "B1004",
@@ -101,7 +101,7 @@ const burgers = [
     manufacturedDate: "2024.09.01",
     expiredDate: "2024.10.01",
     qty: 1,
-    img: "assets/4.jpg",
+    img: "../assets/4.jpg",
   },
   {
     code: "B1005",
@@ -112,7 +112,7 @@ const burgers = [
     manufacturedDate: "2024.09.08",
     expiredDate: "2024.09.28",
     qty: 1,
-    img: "assets/5.jpg",
+    img: "../assets/5.jpg",
   },
   {
     code: "B1006",
@@ -123,18 +123,18 @@ const burgers = [
     manufacturedDate: "2024.08.25",
     expiredDate: "2024.09.25",
     qty: 1,
-    img: "assets/6.jpg",
+    img: "../assets/6.jpg",
   },
   {
     code: "B1007",
     name: "Cheese Burger (Regular)",
     price: 600,
     discount: 0,
-    stock: 9,
+    stock: 0,
     manufacturedDate: "2024.09.05",
     expiredDate: "2024.10.05",
     qty: 1,
-    img: "assets/7.jpg",
+    img: "../assets/7.jpg",
   },
   {
     code: "B1008",
@@ -145,7 +145,7 @@ const burgers = [
     manufacturedDate: "2024.08.30",
     expiredDate: "2024.09.30",
     qty: 1,
-    img: "assets/8.jpg",
+    img: "../assets/8.jpg",
   },
   {
     code: "B1009",
@@ -156,7 +156,7 @@ const burgers = [
     manufacturedDate: "2024.09.12",
     expiredDate: "2024.10.12",
     qty: 1,
-    img: "assets/9.jpg",
+    img: "../assets/9.jpg",
   },
   {
     code: "B1010",
@@ -167,7 +167,7 @@ const burgers = [
     manufacturedDate: "2024.08.15",
     expiredDate: "2024.09.15",
     qty: 1,
-    img: "assets/10.jpg",
+    img: "../assets/10.jpg",
   },
 ];
 
@@ -187,8 +187,8 @@ function createCards(array, parentElement) {
     let product = ` <div class="col">
         <div  class="card shadow-sm" data-bs-toggle="modal"
               data-bs-target="#${element.code}" >
-          <img class="rounded-top" src="${element.img}" alt="hello" />
-          <h5 class="m-2">${element.name}</h5>
+          <img class="rounded-top" src="${element.img}" alt="${element.name}" />
+          <h5 class="m-2" style="color:#d46f10;">${element.name}</h5>
           <p class="m-2">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio cum
           </p>
@@ -291,7 +291,7 @@ function addToCart(code) {
 productDisplay();
 document.getElementById("cartBtn").onclick = function cartBtn() {
   localStorage.setItem("array", JSON.stringify(cartProductList));
-  window.location.href = "pages/cartPage.html";
+  window.location.href = "cartPage.html";
 };
 
 let burgerBtn = document.getElementById("burgerBtn");
